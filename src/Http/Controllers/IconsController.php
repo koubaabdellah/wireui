0bc7da9f3fe303348fbc5a44bba3253e043605c8
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-25 01:04:29 -0300

    refactor the icon controller

--- src/Http/Controllers/IconsController.php
+++ src/Http/Controllers/IconsController.php
@@ -2,22 +2,32 @@
 
 namespace WireUi\Http\Controllers;
 
-use Illuminate\Support\Facades\View;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Response;
+use Throwable;
+use WireUi\View\Components\Icon;
 
 class IconsController extends Controller
 {
-    public function __invoke(string $style, string $icon)
+    public function __invoke(string $icon, ?string $variant = null): Response
     {
-        $view = "wireui::icons.{$style}.{$icon}";
+        try {
+            $component = new Icon(name: $icon, variant: $variant);
 
-        abort_unless(View::exists($view), Response::HTTP_NOT_FOUND, "Icon \"{$icon}\" not found.");
+            return response()
+                ->view($component->render()->name(), ['attributes' => null])
+                ->withHeaders([
+                    'Content-Type'  => 'image/svg+xml; charset=utf-8',
+                    'Cache-Control' => 'public, only-if-cached, max-age=31536000',
+                ]);
+        } catch (Throwable $exception) {
+            if (
+                !str_starts_with($exception->getMessage(), 'View')
+                && !str_ends_with($exception->getMessage(), 'not found.')
+            ) {
+                report($exception);
+            }
 
-        return response()
-            ->view($view, ['attributes' => null])
-            ->withHeaders([
-                'Content-Type'  => 'image/svg+xml; charset=utf-8',
-                'Cache-Control' => 'public, only-if-cached, max-age=31536000',
-            ]);
+            abort(Response::HTTP_NOT_FOUND, "Icon \"{$icon}\" for variant \"{$variant}\" was not found.");
+        }
     }
 }
--- tests/Unit/Controllers/IconsControllerTest.php
+++ tests/Unit/Controllers/IconsControllerTest.php
@@ -1,61 +1,35 @@
 <?php
 
-namespace Tests\Unit\Controllers;
-
-use Illuminate\Support\{Collection, Str};
-use Symfony\Component\Finder\{Finder, SplFileInfo};
 use Tests\Unit\TestCase;
 
-class IconsControllerTest extends TestCase
-{
-    public function test_it_should_assert_icon_is_found()
-    {
-        $this->getJson(route('wireui.icons', ['style' => 'outline', 'icon' => 'user']))
-            ->assertStatus(200)
-            ->assertHeader('Content-Type', 'image/svg+xml; charset=utf-8')
-            ->assertHeader('Cache-Control', 'max-age=31536000, only-if-cached, public')
-            ->assertSee('<svg', escape: false);
-    }
+it('should assert house icon can render', function () {
+    /** @var TestCase $this */
 
-    /**
-     * @test
-     * @dataProvider iconsProvider
-     */
-    public function test_it_should_ensure_($style, $icon)
-    {
-        $this->getJson(route('wireui.icons', ['style' => $style, 'icon' => $icon]))
-            ->assertStatus(200)
-            ->assertHeader('Content-Type', 'image/svg+xml; charset=utf-8')
-            ->assertHeader('Cache-Control', 'max-age=31536000, only-if-cached, public')
-            ->assertSee('<svg', escape: false);
-    }
+    $this->getJson(route('wireui.render.icons', ['variant' => 'regular', 'icon' => 'house']))
+        ->assertStatus(200)
+        ->assertHeader('Content-Type', 'image/svg+xml; charset=utf-8')
+        ->assertHeader('Cache-Control', 'max-age=31536000, only-if-cached, public')
+        ->assertSee('<svg', escape: false);
+});
 
-    public function iconsProvider(): array
-    {
-        return collect([
-            $this->mapIcons('solid'),
-            $this->mapIcons('outline'),
-        ])->collapse()->toArray();
-    }
+it('should assert house icon can render with the default variant', function () {
+    /** @var TestCase $this */
 
-    public function mapIcons(string $style): Collection
-    {
-        $files = (new Finder())->files()->in(__DIR__ . "/../../../resources/views/components/icons/{$style}");
+    $this->getJson(route('wireui.render.icons', ['icon' => 'house']))
+        ->assertStatus(200)
+        ->assertHeader('Content-Type', 'image/svg+xml; charset=utf-8')
+        ->assertHeader('Cache-Control', 'max-age=31536000, only-if-cached, public')
+        ->assertSee('<svg', escape: false);
+});
 
-        return collect($files)->map(fn (SplFileInfo $file) => [
-            'style' => $style,
-            'icon'  => Str::before($file->getFilename(), '.blade.php'),
+it('should assert icon is not found', function () {
+    /** @var TestCase $this */
+
+    $this->getJson(route('wireui.render.icons', ['variant' => 'regular', 'icon' => 'invalid-icon-name']))
+        ->assertStatus(404)
+        ->assertHeader('Content-Type', 'application/json')
+        ->assertHeader('Cache-Control', 'no-cache, private')
+        ->assertExactJson([
+            'message' => 'Icon "invalid-icon-name" for variant "regular" was not found.',
         ]);
-    }
-
-    public function test_it_should_assert_icon_is_not_found()
-    {
-        $this->getJson(route('wireui.icons', ['style' => 'outline', 'icon' => 'invalid-icon-name']))
-            ->assertStatus(404)
-            ->assertHeader('Content-Type', 'application/json')
-            ->assertHeader('Cache-Control', 'no-cache, private')
-            ->assertExactJson([
-                'message' => 'Icon "invalid-icon-name" not found.',
-            ]);
-    }
-}
+});
